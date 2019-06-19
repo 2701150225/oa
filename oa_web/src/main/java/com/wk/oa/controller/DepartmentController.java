5e80a7dafc924058abb0e3a3b023b18bd0946861
王宿生 <1030462238@qq.com>
2019-06-19 23:47:04 +0800

    部门管理表现层部分实现(1)

--- oa_web/src/main/java/com/wk/oa/controller/DepartmentController.java
+++ oa_web/src/main/java/com/wk/oa/controller/DepartmentController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.annotation.Resource;
 import java.util.Map;
 
-@Controller
+@Controller("departmentController")
 @RequestMapping("/department")
 public class DepartmentController {
 
