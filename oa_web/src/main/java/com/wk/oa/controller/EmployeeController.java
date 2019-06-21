d36d031637bd85dfa6ef7eaedad3ff2e89000966
王宿生 <1030462238@qq.com>
2019-06-21 11:07:58 +0800

    员工管理1.2 完成对员工的增删改操作

--- oa_web/src/main/java/com/wk/oa/controller/EmployeeController.java
+++ oa_web/src/main/java/com/wk/oa/controller/EmployeeController.java
@@ -1,6 +1,10 @@
 package com.wk.oa.controller;
 
 
+import com.wk.oa.entity.Department;
+import com.wk.oa.entity.Employee;
+import com.wk.oa.global.Contant;
+import com.wk.oa.service.DepartmentService;
 import com.wk.oa.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +19,48 @@ public class EmployeeController {
     @Resource
     private EmployeeService employeeService;
 
+    @Resource
+    private DepartmentService departmentService;
+
     @RequestMapping("/list")
     public String list(Map<String, Object> map) {
-        map.put("list", employeeService.getAll());
+        map.put("list",employeeService.getAll());
         return "employee_list";
+    }
+
+    @RequestMapping("/to_add")
+    public String toadd(Map<String, Object> map) {
+        map.put("employee", new Employee());
+        map.put("dlist", departmentService.getAll());
+        map.put("plist", Contant.getPosts());
+        return "employee_add";
+    }
+
+    @RequestMapping("/add")
+    public String add(Employee employee) {
+        employeeService.add(employee);
+        return ("redirect:list");
+    }
+
+    @RequestMapping(value = "/to_update", params = "sn")
+    public String toUpdate(String sn, Map<String, Object> map) {
+        map.put("employee", employeeService.get(sn));
+        map.put("dlist", departmentService.getAll());
+        map.put("plist", Contant.getPosts());
+        return "employee_update";
+    }
+
+    @RequestMapping("/update")
+    public String update(Employee employee){
+        employeeService.edit(employee);
+        return "redirect:list";
+    }
+
+    @RequestMapping(value="/remove",params = "sn")
+    public String remove(String sn){
+        employeeService.remove(sn);
+
+        return  "redirect:list";
     }
 
 
