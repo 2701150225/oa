e310312c3ecbb30e81981f6a7305305d76f08744
王宿生 <1030462238@qq.com>
2019-06-24 10:26:38 +0800

    OA系统用户模块3.0 添加修改密码功能

--- oa_web/src/main/java/com/wk/oa/controller/UserController.java
+++ oa_web/src/main/java/com/wk/oa/controller/UserController.java
@@ -31,8 +31,33 @@ public class UserController {
         return "redirect:user";
     }
 
+
     @RequestMapping("/user")
     public String user(){
         return "user";
+    }
+
+    @RequestMapping("/quit")
+    public String quit(HttpSession session){
+        session.setAttribute("employee",null);
+        return "redirect:to_login";
+    }
+
+    @RequestMapping("/to_change_password")
+        public String toChangePassword(){
+        return  "change_password";
+        }
+
+    @RequestMapping("/change_password")
+    public String changePassword(HttpSession session, @RequestParam String oldPassword,@RequestParam String newPassword1,@RequestParam String newPassword2) {
+        Employee employee = (Employee) session.getAttribute("employee");
+        if (employee.getPassword().equals(oldPassword)) {
+            if (newPassword1.equals(newPassword2)) {
+                employee.setPassword(newPassword1);
+                userService.changePassword(employee);
+                return "redirect:to_login";
+            }
+        }
+        return "redirect:to_change_password";
     }
 }
